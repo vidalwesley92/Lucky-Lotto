6fc807b6536443c006762216c3d0805f0457562f
wesley vidal <vidalwesley92@gmail.com>
2024-04-22 02:47:28 +0000

    add: add function to to js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+    let generate = document.getElementsByTagName("button"); 
 
-    
+    for (let button of generate) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "generate") {
+                console.log("generate")
+                //generateNumbers();
+            } else {
+                console.log("reset")
+                //reset();
+            }
+        });
+    }
+
 });
+
+/*
+function generateNumbers() {
+
+    let num1 = Math.floor(Math.random() * 47) + 1;
+    let num2 = Math.floor(Math.random() * 47) + 1;
+    let num3 = Math.floor(Math.random() * 47) + 1;
+    let num4 = Math.floor(Math.random() * 47) + 1;
+    let num5 = Math.floor(Math.random() * 47) + 1;
+    let num6 = Math.floor(Math.random() * 47) + 1;
+}*/
+
+function compareNumbers() {
+
+}
+
+function reset() {
+
+}
