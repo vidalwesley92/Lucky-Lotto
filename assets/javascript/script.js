a857f8c05630d124465a6395346faf28bfcf215a
wesley vidal <vidalwesley92@gmail.com>
2024-04-23 03:14:27 +0000

    fix: fix js behaivor

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,24 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-   
-    function generateNumbers() {
-        let newNumbers = Math.floor(Math.random() * 47) + 1;
-    } 
+document.addEventListener("DOMContentLoaded", function () {
 
-    function generateSixNumbers(){
-        let sixNumbers = [];
-    
-        while(sixNumbers.length<6) {
-            let randomNumbers = generateRandomNumber();
-            if (!sixNumbers.includes(randomNumber)) {
-                sixNumbers.push(randomNumber);
-            }
-        }
-    
-        document.getElementsById("num1").value = sixNumbers[0];
-        document.getElementsById("num2").value = sixNumbers[1];
-        document.getElementsById("num3").value = sixNumbers[2];
-        document.getElementsById("num4").value = sixNumbers[3];
-        document.getElementsById("num5").value = sixNumbers[4];
-        document.getElementsById("num6").value = sixNumbers[5];
+    function generateNumbers() {
+
+        let num1 = Math.floor(Math.random() * 47) + 1;
+        let num2 = Math.floor(Math.random() * 47) + 1;
+        let num3 = Math.floor(Math.random() * 47) + 1;
+        let num4 = Math.floor(Math.random() * 47) + 1;
+        let num5 = Math.floor(Math.random() * 47) + 1;
+        let num6 = Math.floor(Math.random() * 47) + 1;
+
+        return lottoNumbers = [num1, num2, num3, num4, num5, num6];
     }
+
+    function uniqueNumbers() {
+        let unique = 
+    }
+
 })   
